b3a603a57768273f3cd7e4f7b7e53d30e0d790aa
Madi Zhaksykeldi <madizhaksykeldi@Madis-Air-4.Dlink>
2024-12-01 02:47:34 +0500

    sql script

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE weather_db
+-- (
+--     id         BIGSERIAL PRIMARY KEY,
+--     name       VARCHAR(255) NOT NULL,
+--     country    VARCHAR(255) NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
