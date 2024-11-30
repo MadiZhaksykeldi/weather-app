445726e4dae5694df546c4c2445548fe60a3b2e0
Madi Zhaksykeldi <madizhaksykeldi@Madis-Air-4.Dlink>
2024-11-30 20:33:27 +0500

    added exception handler for API

---
+++ src/main/java/kz/madizhaksykeldi/weatherapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package kz.madizhaksykeldi.weatherapp.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<String> handleApiException(ApiException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
