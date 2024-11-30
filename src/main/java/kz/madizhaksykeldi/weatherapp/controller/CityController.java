71b3467a43d6d360500986e9bea096d0a85f546d
Madi Zhaksykeldi <madizhaksykeldi@Madis-Air-4.Dlink>
2024-11-30 20:58:04 +0500

    added Swagger documentation

--- src/main/java/kz/madizhaksykeldi/weatherapp/controller/CityController.java
+++ src/main/java/kz/madizhaksykeldi/weatherapp/controller/CityController.java
@@ -1,5 +1,10 @@
 package kz.madizhaksykeldi.weatherapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kz.madizhaksykeldi.weatherapp.model.City;
 import kz.madizhaksykeldi.weatherapp.service.CityService;
 import lombok.RequiredArgsConstructor;
@@ -13,46 +18,59 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/cities")
 @RequiredArgsConstructor
+@Tag(name = "City Controller", description = "CRUD operations on city")
 public class CityController {
 
     private static final Logger logger = LoggerFactory.getLogger(CityController.class);
     private final CityService cityService;
 
+    @Operation(summary = "Create city", description = "Created new city in database")
+    @ApiResponse(responseCode = "200", description = "City successfully has been created",
+            content = @Content(schema = @Schema(implementation = City.class)))
     @PostMapping
     public ResponseEntity<City> createCity(@RequestBody City city) {
-        logger.info("Запрос на создание города: {}", city);
+        logger.info("Request to create city: {}", city);
         City savedCity = cityService.saveCity(city);
-        logger.info("Город создан: {}", savedCity);
+        logger.info("City created: {}", savedCity);
         return ResponseEntity.ok(savedCity);
     }
 
+    @Operation(summary = "Get city by ID", description = "Returns data about city by ID")
+    @ApiResponse(responseCode = "200", description = "City found",
+            content = @Content(schema = @Schema(implementation = City.class)))
+    @ApiResponse(responseCode = "404", description = "City not found")
     @GetMapping("/{id}")
     public ResponseEntity<City> getCity(@PathVariable Long id) {
-        logger.info("Запрос на получение города с ID: {}", id);
+        logger.info("Request to get city by ID: {}", id);
         return cityService.getCityById(id)
                 .map(city -> {
-                    logger.info("Город найден: {}", city);
+                    logger.info("City found: {}", city);
                     return ResponseEntity.ok(city);
                 })
                 .orElseGet(() -> {
-                    logger.warn("Город с ID {} не найден", id);
+                    logger.warn("City with ID {} not found", id);
                     return ResponseEntity.notFound().build();
                 });
     }
 
+    @Operation(summary = "Get all cities", description = "Returns the list of all cities")
+    @ApiResponse(responseCode = "200", description = "List of city successfully has been retrieved",
+            content = @Content(schema = @Schema(implementation = City.class)))
     @GetMapping
     public ResponseEntity<List<City>> getAllCities() {
-        logger.info("Запрос на получение всех городов");
+        logger.info("Request to get all cities");
         List<City> cities = cityService.getAllCities();
-        logger.info("Найдено городов: {}", cities.size());
+        logger.info("Found cities: {}", cities.size());
         return ResponseEntity.ok(cities);
     }
 
+    @Operation(summary = "Delete city by ID", description = "Deleting city by ID")
+    @ApiResponse(responseCode = "204", description = "City successfully has been deleted")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCity(@PathVariable Long id) {
-        logger.info("Запрос на удаление города с ID: {}", id);
+        logger.info("Request to delete city with ID: {}", id);
         cityService.deleteCity(id);
-        logger.info("Город с ID {} удалён", id);
+        logger.info("City with ID {} deleted", id);
         return ResponseEntity.noContent().build();
     }
 
--- src/main/java/kz/madizhaksykeldi/weatherapp/controller/WeatherController.java
+++ src/main/java/kz/madizhaksykeldi/weatherapp/controller/WeatherController.java
@@ -1,5 +1,10 @@
 package kz.madizhaksykeldi.weatherapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kz.madizhaksykeldi.weatherapp.dto.ForecastDto;
 import kz.madizhaksykeldi.weatherapp.dto.WeatherDto;
 import kz.madizhaksykeldi.weatherapp.exception.ApiException;
@@ -15,38 +20,43 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/weather")
 @RequiredArgsConstructor
+@Tag(name = "Weather Controller", description = "Getting information about weather")
 public class WeatherController {
 
     private static final Logger logger = LoggerFactory.getLogger(WeatherController.class);
     private final WeatherService weatherService;
     private final CityService cityService;
 
+    @Operation(summary = "Get current weather", description = "Returns current weather of city by ID")
+    @ApiResponse(responseCode = "200", description = "Current weather has been retrieved",
+            content = @Content(schema = @Schema(implementation = WeatherDto.class)))
+    @ApiResponse(responseCode = "404", description = "City not found")
     @GetMapping("/current/{cityId}")
     public ResponseEntity<WeatherDto> getCurrentWeather(@PathVariable Long cityId) {
-        logger.info("Получен запрос на текущую погоду для города с ID: {}", cityId);
+        logger.info("Request for current weather of city with ID: {}", cityId);
         City city = cityService.getCityById(cityId)
                 .orElseThrow(() -> {
-                    logger.error("Город с ID {} не найден", cityId);
+                    logger.error("City with iD {} not found", cityId);
                     return new ApiException("City not found");
                 });
-        logger.info("Город найден: {}", city.getName());
+        logger.info("City found: {}", city.getName());
         WeatherDto weather = weatherService.getCurrentWeather(city.getName());
-        logger.debug("Текущая погода для города {}: {}", city.getName(), weather);
+        logger.debug("Current weather of city {}: {}", city.getName(), weather);
         return ResponseEntity.ok(weather);
     }
 
     @GetMapping("/forecast/{cityId}")
     public ResponseEntity<ForecastDto> getWeatherForecast(@PathVariable Long cityId,
                                                           @RequestParam int days) {
-        logger.info("Получен запрос на прогноз погоды для города с ID: {} на {} дней", cityId, days);
+        logger.info("Request for weather forecast of city with ID: {} for {} days", cityId, days);
         City city = cityService.getCityById(cityId)
                 .orElseThrow(() -> {
-                    logger.error("Город с ID {} не найден", cityId);
+                    logger.error("City with ID {} not found", cityId);
                     return new ApiException("City not found");
                 });
-        logger.info("Город найден: {}", city.getName());
+        logger.info("City found: {}", city.getName());
         ForecastDto forecast = weatherService.getWeatherForecast(city.getName(), days);
-        logger.debug("Прогноз погоды для города {} на {} дней: {}", city.getName(), days, forecast);
+        logger.debug("Weather forecast for city{} for {} days: {}", city.getName(), days, forecast);
         return ResponseEntity.ok(forecast);
     }
 
